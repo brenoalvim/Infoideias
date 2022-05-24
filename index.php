90f29407050f441ed69a41ac4808810aa27099ab
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-05-24 13:17:11 -0300

    function complete

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo $seculoAno = seculoAno('1905');
+echo "<hr>";
+echo $seculoAno = seculoAno('1700');
+
+function seculoAno($ano)
+{
+    if ($ano < 1) {
+        return "Ano inválido";
+    } else if ($ano == 1) {
+        return "Século I";
+    }
+    $calc = (($ano - 2) / 100) + 1;
+
+    $seculo = explode(".", $calc);
+
+    return  "Ano " . $ano . " - Século " . $seculo[0];
+}
